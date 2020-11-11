acc74fd75834150fdcb6c825a4195075e10c5771
shanmite <1971513084@qq.com>
2020-11-11 15:03:34 +0800

    test

--- main.js
+++ main.js
@@ -1,2 +1,25 @@
+import {HttpRequest} from './node/HttpRequest';
 const COOKIE = (process.argv.slice(2))[0].substr(7);
-console.log('say: '+COOKIE);
+HttpRequest({
+    type: 'POST',
+    _url: 'https://api.bilibili.com/x/v2/reply/add',
+    contents: {
+        oid:'456295362727813281',
+        type:17,
+        message:`这是我的一小步，却是人类的一大步(Github ation)\n${COOKIE}`,
+        jsonp:'jsonp',
+        csrf:'e12a61f98c9dc9ae26e68ef4e472a833'
+    },
+    headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
+        Accept: 'application/json, text/plain, */*',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
+        Cookie: 'SESSDATA=266867a2%2C1612919172%2C085b8*81; ',
+    },
+    success: chunk => {
+        console.log(chunk);
+    },
+    error: err => {
+        console.log(err);
+    }
+})
