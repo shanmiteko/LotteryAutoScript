b0dc2fab13973eac39a35b349d29ee2b1ec299f2
shanmite <1971513084@qq.com>
2020-11-11 20:37:46 +0800

    test

--- main.js
+++ main.js
@@ -2,13 +2,23 @@
 // /**@type {string}*/
 // const COOKIE = (process.argv.slice(2))[0].substr(7);
 // main(COOKIE);
-import fs from 'fs';
-fs.writeFile('test.txt', 'test',()=>{
-    fs.readFile('test.txt',(err,data)=>{
-        if (err) {
-            console.error(err);
-        } else {
-            console.log(data.toString());
-        }
+// import fs from 'fs';
+const delay = time => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve()
+        }, time)
     })
+};
+const P = new Promise((resolve) => {
+    (async ()=>{
+        for (let index = 0; index < 100; index++) {
+            console.log(new Date(Date.now()).toLocaleString());
+            await delay(1000)
+        }
+        resolve()
+    })()
 });
+P.then(()=>{
+    console.log('运行结束');
+})
