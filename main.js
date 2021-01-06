f348286cc34b386f9d66852ca43811171fe5382f
shanmite <1971513084@qq.com>
2021-01-06 22:22:28 +0800

    fix: secret默认值为 ‘’ !!!

--- main.js
+++ main.js
@@ -12,14 +12,12 @@ const {
 const COOKIE_ARR = [COOKIE, COOKIE_2, COOKIE_3, COOKIE_4, COOKIE_5];
 
 COOKIE_ARR.forEach(async (cookie, num) => {
-    console.log(typeof cookie);
-    console.log(cookie === '');
-    if (typeof cookie === 'undefined' && cookie === '') return;
-    await setGlobalVar(cookie, SCKEY).then(async () => {
+    if (typeof cookie === 'string' && cookie.length > 10) {
+        await setGlobalVar(cookie, SCKEY);
         const { start, isMe, checkCookie } = require("./lib/lottery-in-nodejs");
         const isRight = await checkCookie(num + 1);
         if (!isRight) return;
         await isMe();
         await start();
-    })
+    }
 });
