153d3aa6d9263da5b232f392b8d3a4c4b6f20f3a
shanmite <1971513084@qq.com>
2021-01-06 22:02:21 +0800

    fix_2

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ const COOKIE_ARR = [COOKIE, COOKIE_2, COOKIE_3, COOKIE_4, COOKIE_5];
 
 COOKIE_ARR.forEach(async (cookie, num) => {
     console.log(typeof cookie);
+    console.log(cookie);
     if (typeof cookie === 'undefined') return;
     await setGlobalVar(cookie, SCKEY).then(async () => {
         const { start, isMe, checkCookie } = require("./lib/lottery-in-nodejs");
