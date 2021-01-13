609b412d42f392a5b40892004a6f72dd61af80bb
shanmite <1971513084@qq.com>
2021-01-13 17:47:35 +0800

    feat:存储数据至专栏草稿处

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 const setGlobalVar = require("./lib/setCookie");
 
-const {
-    SCKEY,
-    COOKIE,
-    COOKIE_2,
-    COOKIE_3,
-    COOKIE_4,
-    COOKIE_5,
-} = process.env;
+const { SCKEY } = process.env;
 
-const COOKIE_ARR = [COOKIE, COOKIE_2, COOKIE_3, COOKIE_4, COOKIE_5];
+const COOKIE_ARR = (function () {
+    let ret = []
+    for (let i = 1; i < 10000; i++) {
+        const cookie = process.env['COOKIE_' + i];
+        if (typeof cookie === 'undefined') break;
+        ret.push(cookie);
+    }
+    return ret;
+})()
 
 COOKIE_ARR.forEach(async (cookie, num) => {
     if (typeof cookie === 'string' && cookie.length > 10) {
