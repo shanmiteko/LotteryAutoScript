a46dd3c6745995c2519ba49a4f24032f679ba0a2
shanmite <1971513084@qq.com>
2021-02-21 23:27:32 +0800

    docs: 添加本地运行说明

---
+++ env.example.bat
@@ -0,0 +1,16 @@
+@REM 下载代码到本地
+@REM 安装node(http://nodejs.cn/download/)
+
+@REM for windows
+@REM 此批处理脚本为本地运行代码前设置环境变量时所需
+@REM 使用时先将本文件名改成env.bat
+@REM 填入相应的值
+@REM 需注意 % 要转义为 %%
+@REM npm run test_start 启动脚本
+@REM npm run test_check 检查是否中奖
+@REM npm run test_clear 清空动态和关注
+set COOKIE=
+set NUMBER=1
+set CLEAR=true
+set SCKEY=
+set PUSH_PLUS_TOKEN=
