106f237055704555a21dc879075a9f4453f41a2b
shanmite <1971513084@qq.com>
2021-03-19 12:50:10 +0800

    feat: 阻止同时运行多个抽奖

--- main.js
+++ main.js
@@ -5,13 +5,14 @@ const { NUMBER, CLEAR, COOKIE, PAT, LOCALLAUNCH } = process.env;
 ((async () => {
     if (typeof COOKIE === 'string' && COOKIE.length > 10) {
         if (!LOCALLAUNCH && !PAT) { console.log('请查看README文件, 填入相应的PAT'); return; }
-        await setVariable(COOKIE, PAT);
+        await setVariable(COOKIE);
         const { start, isMe, checkCookie } = require("./lib/lottery-in-nodejs");
         const { clear } = require("./lib/clear");
         const isRight = await checkCookie(NUMBER);
         if (!isRight) return;
         switch (process.argv.slice(2)[0]) {
             case 'start':
+                await setVariable('', PAT);
                 console.log('开始参与抽奖');
                 await start();
                 break;
@@ -20,7 +21,7 @@ const { NUMBER, CLEAR, COOKIE, PAT, LOCALLAUNCH } = process.env;
                 await isMe();
                 break;
             case 'clear':
-                if (typeof CLEAR === 'string' && CLEAR === 'true') {
+                if (CLEAR === 'true') {
                     console.log('开始清理动态');
                     await clear();
                     console.log('清理动态完毕');
