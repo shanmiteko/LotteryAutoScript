62918abf85ee1faf0496014a00079e22f392309b
shanmite <1971513084@qq.com>
2021-05-03 12:23:01 +0800

    fix: 修复无法清理动态

--- main.js
+++ main.js
@@ -6,34 +6,38 @@ try {
     tooltip.log("无env.js文件");
 }
 
+const { setVariable } = require("./lib/setVariable");
+const { start, isMe, checkCookie } = require("./lib/lottery-in-nodejs");
+const { clear } = require("./lib/clear");
+
 ((async () => {
     const { NUMBER, CLEAR, COOKIE, PAT, LOCALLAUNCH } = process.env;
     if (COOKIE) {
-        if (!LOCALLAUNCH && !PAT) { tooltip.log('请查看README文件, 填入相应的PAT'); return; }
-        const { setVariable } = require("./lib/setVariable");
+        if (!LOCALLAUNCH && !PAT) {
+            tooltip.log('请查看README文件, 填入相应的PAT');
+            return;
+        }
         await setVariable(COOKIE);
-        const { start, isMe, checkCookie } = require("./lib/lottery-in-nodejs");
-        const { clear } = require("./lib/clear");
-        const isRight = await checkCookie(NUMBER);
-        if (!isRight) return;
-        switch (process.argv.slice(2)[0]) {
-            case 'start':
-                tooltip.log('开始参与抽奖');
-                await start();
-                break;
-            case 'check':
-                tooltip.log('检查是否中奖');
-                await isMe();
-                break;
-            case 'clear':
-                if (CLEAR === 'true') {
-                    tooltip.log('开始清理动态');
-                    await clear();
-                    tooltip.log('清理动态完毕');
-                }
-                break;
-            default:
-                break;
+        if (await checkCookie(NUMBER)) {
+            switch (process.argv.slice(2)[0]) {
+                case 'start':
+                    tooltip.log('开始参与抽奖');
+                    await start();
+                    break;
+                case 'check':
+                    tooltip.log('检查是否中奖');
+                    await isMe();
+                    break;
+                case 'clear':
+                    if (CLEAR) {
+                        tooltip.log('开始清理动态');
+                        await clear();
+                        tooltip.log('清理动态完毕');
+                    }
+                    break;
+                default:
+                    break;
+            }
         }
     }
     process.exit(0)
