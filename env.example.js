5e3953dea14f6fb813a77ef1bf7d0d08f849acd9
shanmite <1971513084@qq.com>
2021-05-03 19:34:41 +0800

    feat: 本地运行支持多账号

--- env.example.js
+++ env.example.js
@@ -7,41 +7,63 @@
  * `COOKIE` 是必填项
  * `NUMBER` 表示是第几个账号
  * `PAT` 与 `GITHUB_REPOSITORY` 两项是为了从构件中下载已转发信息 可不填
+ * 
+ * 多账号
+ * 将 ENABLE_MULTIPLE_ACCOUNT 的值改为true
+ * 将账号信息依次填写于 MULTIPLE_ACCOUNT 中, 参考例子类推
+ * `WAIT` 表示下一个账号运行等待时间(毫秒)
  */
 const account_parm = {
-    "COOKIE": "",
-    "NUMBER": 1,
-    "CLEAR": true,
-    "LOCALLAUNCH": true,
-    "PAT": "",
-    "GITHUB_REPOSITORY": "用户名/仓库名"
+    COOKIE: "",
+    NUMBER: 1,
+    CLEAR: true,
+    LOCALLAUNCH: true,
+    PAT: "",
+    GITHUB_REPOSITORY: "用户名/仓库名",
+    ENABLE_MULTIPLE_ACCOUNT: false,
+    MULTIPLE_ACCOUNT: JSON.stringify([
+        {
+            COOKIE: "",
+            NUMBER: 1,
+            CLEAR: true,
+            LOCALLAUNCH: true,
+            WAIT: 60 * 1000,
+        },
+        // {
+        //     COOKIE: "",
+        //     NUMBER: 2,
+        //     CLEAR: true,
+        //     LOCALLAUNCH: true,
+        //     WAIT: 60 * 1000,
+        // }
+    ])
 }
 
 /**
  * 推送相关参数
  */
 const push_parm = {
-    "SCKEY=": "",
-    "SENDKEY": "",
-    "QQ_SKEY": "",
-    "QQ_MODE": "",
-    "BARK_PUSH": "",
-    "BARK_SOUND": "",
-    "TG_BOT_TOKEN": "",
-    "TG_USER_ID": "",
-    "TG_PROXY_HOST": "",
-    "TG_PROXY_PORT": "",
-    "DD_BOT_TOKEN": "",
-    "DD_BOT_SECRET": "",
-    "QYWX_KEY": "",
-    "IGOT_PUSH_KEY": "",
-    "PUSH_PLUS_TOKEN": "",
-    "PUSH_PLUS_USER": "",
-    "SMTP_HOST": "",
-    "SMTP_PORT": "",
-    "SMTP_USER": "",
-    "SMTP_PASS": "",
-    "SMTP_TO_USER": ""
+    SCKEY: "",
+    SENDKEY: "",
+    QQ_SKEY: "",
+    QQ_MODE: "",
+    BARK_PUSH: "",
+    BARK_SOUND: "",
+    TG_BOT_TOKEN: "",
+    TG_USER_ID: "",
+    TG_PROXY_HOST: "",
+    TG_PROXY_PORT: "",
+    DD_BOT_TOKEN: "",
+    DD_BOT_SECRET: "",
+    QYWX_KEY: "",
+    IGOT_PUSH_KEY: "",
+    PUSH_PLUS_TOKEN: "",
+    PUSH_PLUS_USER: "",
+    SMTP_HOST: "",
+    SMTP_PORT: "",
+    SMTP_USER: "",
+    SMTP_PASS: "",
+    SMTP_TO_USER: ""
 }
 
 process.env = {
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { tooltip } = require("./lib/Base");
+const { tooltip, delay } = require("./lib/Base");
 
 try {
     require("./env");
@@ -6,39 +6,55 @@ try {
     tooltip.log("无env.js文件");
 }
 
-const { setVariable } = require("./lib/setVariable");
-const { start, isMe, checkCookie } = require("./lib/lottery-in-nodejs");
-const { clear } = require("./lib/clear");
-
-((async () => {
-    const { NUMBER, CLEAR, COOKIE, PAT, LOCALLAUNCH } = process.env;
-    if (COOKIE) {
-        if (!LOCALLAUNCH && !PAT) {
-            tooltip.log('请查看README文件, 填入相应的PAT');
-            return;
-        }
-        await setVariable(COOKIE);
-        if (await checkCookie(NUMBER)) {
-            switch (process.argv.slice(2)[0]) {
-                case 'start':
-                    tooltip.log('开始参与抽奖');
-                    await start();
-                    break;
-                case 'check':
-                    tooltip.log('检查是否中奖');
-                    await isMe();
-                    break;
-                case 'clear':
-                    if (CLEAR) {
-                        tooltip.log('开始清理动态');
-                        await clear();
-                        tooltip.log('清理动态完毕');
+async function main() {
+    const { COOKIE, NUMBER, CLEAR, PAT, LOCALLAUNCH, ENABLE_MULTIPLE_ACCOUNT, MULTIPLE_ACCOUNT } = process.env;
+    if (LOCALLAUNCH || PAT) {
+        if (ENABLE_MULTIPLE_ACCOUNT) {
+            let muti_acco = JSON.parse(MULTIPLE_ACCOUNT);
+            process.env.ENABLE_MULTIPLE_ACCOUNT = '';
+            for (const acco of muti_acco) {
+                process.env.COOKIE = acco.COOKIE;
+                process.env.NUMBER = acco.NUMBER;
+                process.env.CLEAR = acco.CLEAR;
+                await main();
+                await delay(acco.WAIT);
+            }
+        } else {
+            if (COOKIE) {
+                const { setVariable } = require("./lib/setVariable");
+                await setVariable(COOKIE, Number(NUMBER));
+                const { start, isMe, checkCookie } = require("./lib/lottery-in-nodejs");
+                const { clear } = require("./lib/clear");
+                tooltip.log('[LotteryAutoScript] 账号' + NUMBER);
+                if (await checkCookie(NUMBER)) {
+                    switch (process.argv.slice(2)[0]) {
+                        case 'start':
+                            tooltip.log('开始参与抽奖');
+                            await start();
+                            break;
+                        case 'check':
+                            tooltip.log('检查是否中奖');
+                            await isMe();
+                            break;
+                        case 'clear':
+                            if (CLEAR) {
+                                tooltip.log('开始清理动态');
+                                await clear();
+                                tooltip.log('清理动态完毕');
+                            }
+                            break;
+                        default:
+                            break;
                     }
-                    break;
-                default:
-                    break;
+                }
             }
         }
+    } else {
+        tooltip.log('请查看README文件, 填入相应的PAT');
     }
+}
+
+(async function () {
+    await main();
     process.exit(0)
-}))();
+})()
