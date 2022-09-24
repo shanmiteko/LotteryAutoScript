468300227d10b72667ace5417c9143a8db69aac2
shanmite <shanmite@hotmail.com>
2022-09-24 20:35:29 +0800

    fix: 青龙执行脚本时出错(#202)

--- script/qinglong/init.sh
+++ script/qinglong/init.sh
@@ -2,6 +2,8 @@
 set -e
 
 NAME=LotteryAutoScript
+
+# 视网络情况选择链接
 GIT_REPO=https://github.com/shanmiteko/${NAME}.git
 # GIT_REPO=https://ghproxy.com/https://github.com/shanmiteko/${NAME}.git
 
@@ -14,11 +16,13 @@ else
 fi
 
 if [ -f "$NAME/my_config.js" ]; then
+    echo 'my_config 已存在'
 else
     cp $NAME/my_config.example.js $NAME/my_config.js
 fi
 
 if [ -f "$NAME/env.js" ]; then
+    echo 'env.js 已存在'
 else
     cp $NAME/env.example.js $NAME/env.js
 fi
