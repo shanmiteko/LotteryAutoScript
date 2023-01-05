92f1ad2085eed7cddc35e07144b634624db9ca15
shanmite <shanmite@hotmail.com>
2023-01-05 10:19:34 +0800

    fix: 自动评论没有正确评论所有抽奖 (#235)
    Fixed #235

--- test/api.test.js
+++ test/api.test.js
@@ -6,9 +6,11 @@ const util = require('./util');
     assert(await bili_client.getMyinfo());
 
     await util.par_run([0, 1, 2, 3, 4], [
+        // 0
         async () => {
             assert.equal((await bili_client.getTopRcmd()).length, 10)
         },
+        // 1
         async () => {
             assert.equal(await bili_client.sendChat(
                 (await bili_client.getOneDynamicByDyid("692193323569381399")).desc.rid,
@@ -17,6 +19,7 @@ const util = require('./util');
                 7
             )
         },
+        // 2
         async () => {
             assert.equal(await bili_client.sendChat(
                 (await bili_client.getOneDynamicByDyid("11229466874154064")).desc.rid,
@@ -25,12 +28,15 @@ const util = require('./util');
                 3
             )
         },
+        // 3
         async () => {
             assert.notEqual((await bili_client.searchArticlesByKeyword("专栏")).length, 0)
         },
+        // 4
         async () => {
             assert.notEqual(await bili_client.sendChat("703886913053917267", "t", 17), 1)
         },
+        // 5
         async () => {
             assert(!await bili_client.createDynamic("1"))
         }
--- test/dynamic_card.test.js
+++ test/dynamic_card.test.js
@@ -6,19 +6,30 @@ const util = require('./util');
 (async () => {
     assert(await bili_client.getMyinfo());
 
-    await util.par_run([0, 1, 2], [
+    await util.par_run([0, 1, 2, 3], [
+        // 0
         async () => {
             let info = await bili_client.getOneDynamicByDyid("728424890210713624");
             assert(searcher.parseDynamicCard(info).is_charge_lottery);
         },
+        // 1
         async () => {
             let info = await bili_client.getOneDynamicByDyid("728455586333589522");
             assert(searcher.parseDynamicCard(info).origin_is_charge_lottery);
         },
+        // 2
         async () => {
             let card = searcher.parseDynamicCard(await bili_client.getOneDynamicByDyid("746824225190314008"));
             let chats = await bili_client.getChat(card.rid_str, card.chat_type)
             assert(chats.length > 0 && typeof chats[0] == "string")
+        },
+        // 3
+        async () => {
+            let card = searcher.parseDynamicCard(await bili_client.getOneDynamicByDyid("747169355882561625"));
+            assert(card.chat_type == 11)
+            card = searcher.parseDynamicCard(await bili_client.getOneDynamicByDyid("747441158580338693"));
+            assert(card.chat_type == 17)
+            assert(card.origin_chat_type == 11)
         },
     ])
 
